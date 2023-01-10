1e19b8f4adb469343c2b6f79391acb96a695658d
Amanda Campos <amandaolivcampos@gmail.com>
2023-01-10 15:06:58 -0300

    Test (add two positive numbers)

--- src/main/java/org/example/entities/Calculator.java
+++ src/main/java/org/example/entities/Calculator.java
@@ -2,6 +2,7 @@ package org.example.entities;
 
 public class Calculator {
 
+
     public int add(int a, int b) {
         return a + b;
     }
--- src/test/java/org/example/entities/CalculatorTest.java
+++ src/test/java/org/example/entities/CalculatorTest.java
@@ -4,9 +4,12 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 public class CalculatorTest extends TestCase {
+
+
     @Test
-    public void testAdd() {
-
-
+    public void add_two_positive_numbers() {
+        Calculator calculator = new Calculator();
+        int actual = calculator.add(3, 7);
+        assertEquals(10, actual);
     }
 }
