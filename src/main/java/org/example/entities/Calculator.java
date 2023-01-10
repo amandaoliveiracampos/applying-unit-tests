d7dd6be390f0c525855c74200c3603d2124fdf1e
Amanda Campos <amandaolivcampos@gmail.com>
2023-01-10 14:29:07 -0300

    creating calculator class

---
+++ src/main/java/org/example/entities/Calculator.java
@@ -0,0 +1,8 @@
+package org.example.entities;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/org/example/entities/CalculatorTest.java
@@ -0,0 +1,12 @@
+package org.example.entities;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class CalculatorTest extends TestCase {
+    @Test
+    public void testAdd() {
+
+
+    }
+}
